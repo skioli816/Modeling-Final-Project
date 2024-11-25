b41d6c0de2acb8cf0fb1ca1c3ccc8081f7d9667d
skioli816 <sscioli3@gatech.edu>
2024-11-25 15:53:26 -0500

    Update MatlabCode.m

--- MatlabCode.m
+++ MatlabCode.m
@@ -1,19 +1,38 @@
-% Sarah's PS5 Code that doesn't bug but is also definitely wrong (Part (a)) 
 % Set Parameters 
-u = 1 ; % m/s 
-x = [0:1:2e3] ; % km 
-t = [0: 1: 1000] ; % yrs 
-dt = 1 ; % (yr) Chosen so that C will equal one and the solution will be accurate & stable 
-dx = 1 ; % (km) Chosen so that C will equal one and the solution will be accurate & stable
-C = u*(dt/dx) ; 
-n = 2000 ; 
-m = t(1001) / dt ; 
-s = exp(((0.0001*0) - 30)*20) ; 
+g = 9.81 ; % gravity (m/s^2) 
+S = ; % bed slope 
+Cf = 1 ; % drag coefficient 
+k = 1.5 ; 
+h = ; % lahar thickness (m) 
+u = ; % horizontal velocity (m/s) 
+beta = sqrt(g*S/Cf) ; % (m^1/2 / s) 
+v = beta*sqrt(h) ; vertical velocity (m/s)
+q = v*h ; % volumetric flow rate (m^3 / s) 
+
+% Time Steps 
+t0 = 0 ; % initial time (s)
+tf = ; % total time (s) 
+dt = ; % time step (s) 
+n = 1:(tf) ; % number of time steps 
+tn = t0 + n*dt ; 
+
+% Space Steps 
+x0 = 0 ; % initial space (m) 
+dx = ; % space step (m) 
+L = ; % length of domain (m) 
+i = 1:(L/dx) ; % number of space steps 
+xi = x0 + i*dx ; 
+
+% Space Relative to Time 
+C = (u*dt) / dx ; % Courant Number (Fraction of space step that a signal has traveled during a time step) 
+
+% Primary Equation 
+% (dh/dt) + beta*k*(h^(k-1))*(dh/dx) = 0 ; 
 
 % Preallocate 
-Jarray = nan.*ones(n, m) ; 
-Jarray(1:n, 1) = (((2e3) / dx) - 1)*s ; 
-J = Jarray(:, 1) ; 
+Aarray = nan.*ones(n, m) ; 
+Aarray(1:n, 1) = (((2e3) / dx) - 1)*s ; 
+A = Aarray(:, 1) ; 
 
 % Evolution Matrix 
 data = [C*ones(n), (1-C)*ones(n)] ; 
@@ -26,10 +45,9 @@ M(2e3, 1000) = s ;
 
 % Model Loop 
 for i = 1:m   
-    Jnew = (J).* M ; 
-    Jarray(:, 1000) = Jnew(:, 212) ; 
-    J = Jnew ; 
-    % Jarray(i, k+1) = Jarray(i, k) - C*(Jarray(i, k) - Jarray(i-1, k)); % Marching Equation 
+    Anew = (A).* M ; 
+    Aarray(:, 1000) = Anew(:, 212) ; 
+    A = Anew ; 
 end 
 
 % FYI I had no idea how to do the Neuman boundary conditions so that part I totally was winging it 
