f433c67ae2ef67ada130f28d0aef5427d2dc9e02
skioli816 <sscioli3@gatech.edu>
2024-11-25 16:19:54 -0500

    Update MatlabCode.m

--- MatlabCode.m
+++ MatlabCode.m
@@ -1,32 +1,42 @@
 % Set Parameters 
 g = 9.81 ; % gravity (m/s^2) 
-S = 1e-5; % bed slope (m/m) 
-Cf = 1 ; % drag coefficient 
+S = 0.1 ; % bed slope (m/m) 
+Cf = 10 ; % drag coefficient 
 k = 1.5 ; 
 u = 0.536; % horizontal velocity (m/s) 
 beta = sqrt(g*S/Cf) ; % (m^1/2 / s) 
+
+% Time Steps 
+t0 = 0 ; % initial time (s)
+dt = 3600 ; % time step (s) 
+tf = 1.21e6 ; % total time (s) 
+n = tf/dt ; % number of time steps 
+t = nan.*ones(1, tf) 
+t(1) = t0 ; 
+for j = 1:n 
+    t(j) = t0 + n*dt ; 
+end 
+
+% Space Steps 
+x0 = 0 ; % initial space (m) 
+dx = 1 ; % space step (m) 
+L = 5000 ; % length of domain (m) 
+m = L/dx ; % number of space steps 
+x = nan.*ones(1, L) ; 
+x(1) = x0 ; 
+for i = 1:m 
+    x(i+1) = x0 + i*dx ; 
+end    
+
+
+% Space Relative to Time 
+C = (u*dt) / dx ; % Courant Number (Fraction of space step that a signal has traveled during a time step) 
 
 % Define Variables 
 h = ; % lahar thickness (m) 
 v = beta*sqrt(h) ; % vertical velocity (m/s)
 q = v*h ; % volumetric flow rate (m^3 / s) 
 
-% Time Steps 
-t0 = 0 ; % initial time (s)
-tf = ; % total time (s) 
-dt = ; % time step (s) 
-n = 1:(tf) ; % number of time steps 
-tn = t0 + n*dt ; 
-
-% Space Steps 
-x0 = 0 ; % initial space (m) 
-dx = ; % space step (m) 
-L = ; % length of domain (m) 
-i = 1:(L/dx) ; % number of space steps 
-xi = x0 + i*dx ; 
-
-% Space Relative to Time 
-C = (u*dt) / dx ; % Courant Number (Fraction of space step that a signal has traveled during a time step) 
 
 % Primary Equation 
 % (dh/dt) + beta*k*(h^(k-1))*(dh/dx) = 0 ; 
